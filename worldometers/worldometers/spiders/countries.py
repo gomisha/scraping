6104afb9b8b41177cc7b101e73373da632f4b09f
Misha Rybalov <Misha_Rybalov@symantec.com>
2020-02-03 06:53:32 -0500

    added population of each country by year

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import logging
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
@@ -13,7 +13,16 @@ class CountriesSpider(scrapy.Spider):
         for country in country_rows:
             name = country.xpath(".//text()").get()
             link = country.xpath(".//@href").get()
+            yield response.follow(url = link, callback = self.parse_country, meta={'country_name': name})
+
+    def parse_country(self, response):
+        name = response.request.meta['country_name']
+        rows = response.xpath("(//table[@class=\"table table-striped table-bordered table-hover table-condensed table-list\"])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
             yield {
-                'name': name,
-                'link': link
+                'country': name,
+                'year' : year,
+                'population': population
             }
