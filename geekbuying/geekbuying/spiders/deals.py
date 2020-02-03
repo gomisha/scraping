22efe74bf0d046b2eb4976c19f4c96c6a6884039
Misha Rybalov <Misha_Rybalov@symantec.com>
2020-02-03 18:13:15 -0500

    geek buying - added pagination

--- geekbuying/geekbuying/spiders/deals.py
+++ geekbuying/geekbuying/spiders/deals.py
@@ -4,17 +4,17 @@ import scrapy
 
 class DealsSpider(scrapy.Spider):
     name = 'deals'
-    allowed_domains = ['www.geekbuying.com/deals']
+    allowed_domains = ['www.geekbuying.com']
     start_urls = ['https://www.geekbuying.com/deals']
 
     def parse(self, response):
-        products = response.xpath("//div[@class=\"category_li\"]")
+        products = response.xpath("//div[@class='category_li']")
         for product in products:
             product_name = product.xpath(".//a[@class='category_li_link']/text()").get()
             product_price = product.xpath(".//div[@class='category_li_price']/span/text()").get()
             product_url = product.xpath(".//a[@class='category_li_link']/@href").get()
             quantity = product.xpath(".//div[@class='category_li_clai']/div/span/text()").get()
-        
+
             yield {
                 'name': product_name,
                 'price': product_price,
@@ -22,4 +22,6 @@ class DealsSpider(scrapy.Spider):
                 'quantity': quantity
             }
 
-        #print(response.body)
+        next_page_link = response.xpath("//a[@class='next']/@href").get()
+        if next_page_link:
+            yield response.follow(url=next_page_link, callback=self.parse)
