2498c0dc61f6401f352c9a055d45fe0fba7c46c6
Misha Rybalov <Misha_Rybalov@symantec.com>
2020-02-04 05:02:54 -0500

    user agent spoofing in settings.py

--- geekbuying/geekbuying/settings.py
+++ geekbuying/geekbuying/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'geekbuying.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'geekbuying (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- geekbuying/geekbuying/spiders/deals.py
+++ geekbuying/geekbuying/spiders/deals.py
@@ -4,12 +4,7 @@ import scrapy
 class DealsSpider(scrapy.Spider):
     name = 'deals'
     allowed_domains = ['www.geekbuying.com']
-    #start_urls = ['https://www.geekbuying.com/deals']
-
-    def start_requests(self):
-        yield scrapy.Request(url='https://www.geekbuying.com/deals', headers={
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
-        })
+    start_urls = ['https://www.geekbuying.com/deals']
 
     def parse(self, response):
         products = response.xpath("//div[@class='category_li']")
@@ -30,6 +25,4 @@ class DealsSpider(scrapy.Spider):
 
         next_page_link = response.xpath("//a[@class='next']/@href").get()
         if next_page_link:
-            yield response.follow(url=next_page_link, callback=self.parse, headers={
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
-            })
+            yield response.follow(url=next_page_link, callback=self.parse)
