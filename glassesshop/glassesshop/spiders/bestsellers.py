81e357a404adb4fd119552d7afb237f2640e4481
Misha Rybalov <Misha_Rybalov@symantec.com>
2020-02-04 16:50:05 -0500

    glasses shop - refactored price scrape into method

--- glassesshop/glassesshop/spiders/bestsellers.py
+++ glassesshop/glassesshop/spiders/bestsellers.py
@@ -7,23 +7,27 @@ class BestsellersSpider(scrapy.Spider):
     allowed_domains = ['www.glassesshop.com']
     start_urls = ['https://www.glassesshop.com/bestsellers']
 
+    def get_price(self, selector):
+        price = selector.xpath(".//div[@class='row']/div[contains(@class, 'pprice')]/span/text()").get()
+        # sale price will be in different place and regular price will be blank
+        price = price.strip() #sale price will cause this to have whitespace and be blank
+        if len(price) < 5:
+            #sale price
+            price = selector.xpath(".//div[2]/div/span/span/text()").get()
+        return price
+
+
     def parse(self, response):
         glasses_all = response.xpath("//div[contains(@class, 'm-p-product')]")
 
         for glasses in glasses_all:
             name = glasses.xpath(".//div[@class='row']/p[contains(@class, 'pname')]/a/text()").get()
-            price = glasses.xpath(".//div[@class='row']/div[contains(@class, 'pprice')]/span/text()").get()
 
             #promo ad - "buy 1 get 1 free" instead of glasses shown sometimes in grid
             if name is None:
                  continue
 
-            # sale price will be in different place and regular price will be blank
-            price = price.strip() #sale price will cause this to have whitespace and be blank
-            if len(price) < 5:
-                #sale price
-                price = glasses.xpath(".//div[2]/div/span/span/text()").get()
-
+            price = self.get_price(glasses)
             link = glasses.xpath(".//div[2]/p/a/@href").get()
             image = glasses.xpath(".//div[1]/a/img[1]/@src").get()
 
