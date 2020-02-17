f20cd88c63b1392e383838da359cd10fd4be591f
gomisha <misha@gomisha.com>
2020-02-17 07:26:31 -0500

    Puppeteer - Zillow stealth updated

--- puppeteer/zillow_stealth.js
+++ puppeteer/zillow_stealth.js
@@ -5,27 +5,18 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
 puppeteer.launch({ headless: false }).then(async browser => {
+    let myUserAgent = userAgent.random();
 
-//   const userAgent = new UserAgent();
-//   console.log(userAgent.toString());
-//   console.log(JSON.stringify(userAgent.data, null, 2));
+    console.log(`userAgent: ${myUserAgent.toString()}`);
 
-//   console.log(`userAgent1: ${userAgent.toString()}`);
-//   console.log(`userAgent2: ${userAgent.toString()}`);
-//   console.log(`userAgent3: ${userAgent.toString()}`);
-//   console.log(JSON.stringify(userAgent.data, null, 2));
+    const page = await browser.newPage()
+    await page.setUserAgent(myUserAgent.toString())
 
+    console.log(`Testing the stealth plugin..`)
+    await page.goto('https://zillow.com')
 
-  const page = await browser.newPage()
-  await page.setUserAgent(userAgent.toString())
+    await page.screenshot({ path: 'zillow-stealth.png' })
 
-  await page.setViewport({ width: 800, height: 600 })
-
-  console.log(`Testing the stealth plugin..`)
-  await page.goto('https://zillow.com')
-  await page.waitFor(5000)
-  await page.screenshot({ path: 'zillow-stealth.png', fullPage: true })
-
-  console.log(`All done, check the screenshots. ✨`)
-  await browser.close()
+    console.log(`All done, check the screenshots. ✨`)
+    await browser.close()
 })
